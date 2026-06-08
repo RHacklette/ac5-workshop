5698daf87c86a7bb65c2c1f300bdce0ec99b9cb9
RHacklette <valou7471@gmail.com>
2026-06-08 10:22:53 +0000

    feat(arista): rev0.2

--- containers/lab/.devcontainer/entrypoint.sh
+++ containers/lab/.devcontainer/entrypoint.sh
@@ -21,3 +21,15 @@ if [ -z "${CODE_SERVER_BIND_ADDR}" ]; then
     CODE_SERVER_BIND_ADDR="0.0.0.0:5000"
 fi
 code-server --bind-addr ${CODE_SERVER_BIND_ADDR} --auth password --disable-telemetry --disable-update-check --disable-workspace-trust "${CONTAINERWSF}" &
+
+# run magic moby script for D-in-D
+/usr/local/share/docker-init.sh
+
+# check if ceos-lab image already present
+if [ -z "$(${CONTAINER_ENGINE} image ls | grep 'arista/ceos')" ]; then
+    docker load < ceos_lab.tar.gz
+    echo "WARNING: cEOS-lab image was successfully loaded."
+fi
+
+# start the lab
+make start
